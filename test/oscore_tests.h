1fc043e0996145fa2aaffad74514ecab5500e23a
Stefan Hristozov <stefan.hristozov@eriptic.com>
2022-11-18 17:46:31 +0100

    change test function name only

--- test/oscore_tests.h
+++ test/oscore_tests.h
@@ -19,7 +19,7 @@ void t2_oscore_server_request_response(void);
 void t4_oscore_server_key_derivation(void);
 void t6_oscore_server_key_derivation(void);
 void t8_oscore_server_response_simple_ack(void);
-void t9_oscore_client_server_registration_two_notifications_cancellation(void);
+void t9_oscore_client_server_observe(void);
 void t10_oscore_client_server_after_reboot(void);
 
 /*unit tests*/
