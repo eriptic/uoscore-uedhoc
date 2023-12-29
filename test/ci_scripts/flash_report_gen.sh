b62e8a55198fa246432b5b6782fc430bf3dc5e0c
Stefan Hristozov <stefan.hristozov@eriptic.com>
2023-12-29 12:44:32 +0100

    test autometed ROM report generation

--- test/ci_scripts/flash_report_gen.sh
+++ test/ci_scripts/flash_report_gen.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
+#set -x #echo on
 
 # run this script from the "test" directory after Zephyr is initialized with:
 # cd <to test dir>
 # ./ci_scripts/flash_report_gen.sh
 # the script produces build_reports/flash_report.txt
 
+echo "\n"
+echo "*********************************************************************\n"
+echo "start flash_report_gen.sh\n"
+echo "*********************************************************************\n"
 mkdir -p build_reports
 rm -rf build_reports/rom_report_nrf91_with_tinycrypt.txt
 
@@ -15,10 +20,11 @@ rm -rf build_reports/rom_report_nrf91_with_tinycrypt.txt
 rm -rf build
 west build -b nrf9160dk_nrf9160 -- -DCOMMAND_LINE_FLAGS="-DMESSAGE_4 -DTINYCRYPT"
 west build -t rom_report > build_reports/rom_report_nrf91_with_tinycrypt.txt
+echo "\n\n\n"
 
 # MBEDTLS     YES
 # MESSAGE_4:    YES
 # VLA:          YES
-rm -rf build
-west build -b nrf9160dk_nrf9160 -- -DCOMMAND_LINE_FLAGS="-DMESSAGE_4 -DMBEDTLS"
-west build -t rom_report > build_reports/rom_report_nrf91_with_mbedtls.txt
+#rm -rf build
+#west build -b nrf9160dk_nrf9160 -- -DCOMMAND_LINE_FLAGS="-DMESSAGE_4 -DMBEDTLS"
+#west build -t rom_report > build_reports/rom_report_nrf91_with_mbedtls.txt
