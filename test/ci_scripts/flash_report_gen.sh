6269b94165afe2eaf4a90fb7598dbd464d4cdf96
Stefan Gloor <stefan.gloor@siemens.com>
2026-01-16 11:32:55 +0100

    chore: replace nrf9160dk_nrf9160 with nrf9160/nrf9160
    The board was renamed. This should fix the error:
    
    No board named 'nrf9160dk_nrf9160' found. Did you mean:
    
    nrf9160dk
    nrf9161dk
    
    Run 'west boards' for the full list.
    CMake Error at
    /home/runner/zephyrproject/zephyr/cmake/modules/boards.cmake:221
    (message): Invalid BOARD; see above.
    
    Signed-off-by: Stefan Gloor <stefan.gloor@siemens.com>

--- test/ci_scripts/flash_report_gen.sh
+++ test/ci_scripts/flash_report_gen.sh
@@ -18,7 +18,7 @@ rm -rf build_reports/rom_report_nrf91_with_tinycrypt.txt
 # MESSAGE_4:    YES
 # VLA:          YES
 rm -rf build
-west build -b nrf9160dk_nrf9160 -- -DCOMMAND_LINE_FLAGS="-DMESSAGE_4 -DTINYCRYPT"
+west build -b nrf9160dk/nrf9160 -- -DCOMMAND_LINE_FLAGS="-DMESSAGE_4 -DTINYCRYPT"
 west build -t rom_report > build_reports/rom_report_nrf91_with_tinycrypt.txt
 echo "\n\n\n"
 
@@ -26,5 +26,5 @@ echo "\n\n\n"
 # MESSAGE_4:    YES
 # VLA:          YES
 rm -rf build
-west build -b nrf9160dk_nrf9160 -- -DCOMMAND_LINE_FLAGS="-DMESSAGE_4 -DMBEDTLS"
+west build -b nrf9160dk/nrf9160 -- -DCOMMAND_LINE_FLAGS="-DMESSAGE_4 -DMBEDTLS"
 west build -t rom_report > build_reports/rom_report_nrf91_with_mbedtls.txt
